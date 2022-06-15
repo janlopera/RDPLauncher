bf0234b5f6537a3c89bb225ee7e106a7560cc086
Jan Cano Lopera <jlopera@edu.tecnocampus.cat>
2022-06-15 17:28:33 +0200

    Initial commit

---
+++ RDPLauncher/ConnectionModel.cs
@@ -0,0 +1,8 @@
+﻿namespace RDPLauncher;
+
+public class ConnectionModel
+{
+    public string IpAddress { get; set; }
+    public string Port { get; set; }
+    public string ExtraParams { get; set; }
+}
---
+++ RDPLauncher/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Http.Json;
+using CliWrap;
+using RDPLauncher;
+
+string baseUrl = "http://10.0.1.5";
+
+HttpClient client = new HttpClient();
+var res = await client.GetAsync($"{baseUrl}/Connect");
+
+if (res.IsSuccessStatusCode)
+{
+    var conn = await res.Content.ReadFromJsonAsync<ConnectionModel>();
+
+    Console.WriteLine($"Conn: {conn.IpAddress}");
+
+    var result = await Cli.Wrap("/usr/local/bin/xfreerdp")
+        .WithArguments($"/v:{conn!.IpAddress}:{conn!.Port} /f /u:admin /p:admin /d:  /network:auto /rfx /multimon /rfx-mode:video /cert:ignore")
+        .WithValidation(CommandResultValidation.None)
+        .ExecuteAsync();
+
+    Console.WriteLine($"Uwu {result.ExitCode}");
+
+    await client.PostAsync($"{baseUrl}/Release", JsonContent.Create<ConnectionModel>(conn));
+
+
+    //await Cli.Wrap("systemctl poweroff").ExecuteAsync();
+}
